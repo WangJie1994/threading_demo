833b0aafb1c519ee547c77c2f5cde1648c8ca154
Jie <wangjie19940416@icloud.com>
2018-05-15 23:38:37 +0800

    Initial commit

---
+++ 添加线程.py
@@ -0,0 +1,15 @@
+import threading
+
+def thread_job():
+    print('This is a added thread, number is %s' % threading.current_thread())
+
+
+def main():
+    added_thread = threading.Thread(target=thread_job())
+    added_thread.start()
+    print(threading.active_count())
+    print(threading.enumerate())
+    print(threading.current_thread())
+
+if __name__ == '__main__':
+    main()
